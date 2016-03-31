42eedfd8c74df5501301b0de6112cc56243174b7
Aneesh <Aneesh@10.215.25.116>
2016-04-01 01:49:53 +0530

    Updates

---
+++ PractiseGot/src/Basics/OperatorExample.java
@@ -0,0 +1,42 @@
+package Basics;
+
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10%3);
+int numberOfDaysCount = 10;
+
+		
+ 		/*System.out.println("numberOfDaysCount " + numberOfDaysCount);
+		numberOfDaysCount++;
+		System.out.println("numberOfDaysCount " + numberOfDaysCount);
+		numberOfDaysCount = numberOfDaysCount+1;
+		System.out.println("numberOfDaysCount " + numberOfDaysCount);
+		numberOfDaysCount = numberOfDaysCount+1;
+		System.out.println("numberOfDaysCount " + numberOfDaysCount);*/
+
+/*		System.out.println("numberOfDaysCount " + numberOfDaysCount++);
+		System.out.println("numberOfDaysCount " + numberOfDaysCount++);
+		System.out.println("numberOfDaysCount " + numberOfDaysCount++);
+		System.out.println("numberOfDaysCount " + numberOfDaysCount++);
+*/
+		/*
+		System.out.print(++numberOfDaysCount +" ");
+		System.out.print(++numberOfDaysCount +" ");
+		System.out.print(++numberOfDaysCount+" ");
+		System.out.print(++numberOfDaysCount+" ");
+		*/
+		
+		
+		System.out.println("numberOfDaysCount " + numberOfDaysCount);
+		numberOfDaysCount = numberOfDaysCount+2;
+		System.out.println("numberOfDaysCount " + numberOfDaysCount);
+		numberOfDaysCount += 2;
+		System.out.println("numberOfDaysCount " + numberOfDaysCount);
+		numberOfDaysCount = numberOfDaysCount+2;
+		System.out.println("numberOfDaysCount " + numberOfDaysCount);
+	}
+
+}
---
+++ PractiseGot/src/Basics/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Aneesh
+ *
+ */
+package Basics;
